e40f2928b55d3342d7be2ddedbb2c84c47546cbf
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-02-19 22:44:54 -0500

    Add files via upload
    add new alien shot class

---
+++ alShot.js
@@ -0,0 +1,24 @@
+class Shot2 {
+    constructor(){
+    this.radius=2;
+    this.pos=createVector(alien.pos.x,alien.pos.y);
+
+    this.vel=p5.Vector.sub(tri.pos,this.pos);
+    this.vel=this.vel.normalize();
+    this.vel.mult(4);
+    
+   // Shot 
+    }         
+    update(){      //Shot
+    this.pos.add(this.vel);
+  //  this.vel.mult(1.08); 
+    
+    } 
+    show(){    //Shot
+     push();
+    translate(this.pos.x,this.pos.y);
+    noStroke();   
+    ellipse(0,0, this.radius*3);
+    pop();
+    } 
+}
