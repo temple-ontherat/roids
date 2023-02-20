51adc2fb1fd5b831b9e2662b07768b68c10de452
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-02-19 22:56:59 -0500

    Update alien.js
    added alien image

--- alien.js
+++ alien.js
@@ -2,15 +2,30 @@ class Alien {
     constructor(w,h) {
     this.width=w;
     this.height=h;
+    this.radius=(this.width+this.height)/2
     this.pos= createVector(random(0,width),10);
     this.vel= createVector(random(-5,5),random(0,5));
+    this.vel.normalize();
+    this.vel.mult(4);
+    this.colorR=255;
+    this.colorB=255;
+    this.colorC=255;
+    this.alf=255;
     }
     update() {
     this.pos.add(this.vel);
     }
     show() {
     push();
+   // fill('rgba(this.color,this.alf)');
+   // colorMode(RGB);
+    // fill((this.color,this.alf));
+    noStroke();
+    fill(this.colorR,this.colorB,this.colorC,this.alf);
     ellipse(this.pos.x,this.pos.y,this.width,this.height);
+    imageMode(CENTER);
+    image(image4,this.pos.x,this.pos.y,this.width,this.height);
+    //line(this.pos.x,this.pos.y,tri.pos.x,tri.pos.y);
     pop();
     }
     edges() {
