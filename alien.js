5a58e8ab36d98b5a3cba77727ae412c313a72bd9
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-02-24 17:02:37 -0500

    Update alien.js
    added image

--- alien.js
+++ alien.js
@@ -8,9 +8,13 @@ class Alien {
     this.vel.normalize();
     this.vel.mult(4);
     this.colorR=255;
+    this.colorG=255;
     this.colorB=255;
-    this.colorC=255;
+    this.colorR2=0;
+    this.colorG2=0;
+    this.colorB2=0;
     this.alf=255;
+    this.alf2=255;
     }
     update() {
     this.pos.add(this.vel);
@@ -21,10 +25,18 @@ class Alien {
    // colorMode(RGB);
     // fill((this.color,this.alf));
     noStroke();
-    fill(this.colorR,this.colorB,this.colorC,this.alf);
+    // fill(this.colorR,this.colorG,this.colorB,this.alf);
+   
+    fill(this.colorR,this.colorG,this.colorB,this.alf);
+
     ellipse(this.pos.x,this.pos.y,this.width,this.height);
+    // fill(this.colorR2,this.colorB2,this.colorB2,this.alf2);
+    // ellipse(this.pos.x,this.pos.y,this.width-15,this.height-10);
     imageMode(CENTER);
-    image(image4,this.pos.x,this.pos.y,this.width,this.height);
+    tint(this.colorR,this.colorG,this.colorB,this.alf);
+    image(image5,this.pos.x,this.pos.y,this.width+30,this.height+20);
+    // image(image4,this.pos.x,this.pos.y,this.width,this.height);
+
     //line(this.pos.x,this.pos.y,tri.pos.x,tri.pos.y);
     pop();
     }
