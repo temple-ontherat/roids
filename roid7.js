6e14a74e2ed2ddee32c846f32ca060bcb1ff84af
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-03-01 23:27:59 -0500

    Update roid7.js
    Added more random asteroid creation

--- roid7.js
+++ roid7.js
@@ -1,16 +1,24 @@
 class Roid {
-  constructor(rad,x,y){
+  constructor(rad,x,y,mult){
     this.radius=rad;
     this.x=x;
     this.y=y;
-    this.pos=createVector(random(0,width/4),random(0,height/4))
+    // this.pos=createVector(random(0,width/4),random(0,height/4));
+    this.pos=createVector(random(0,width),random(0,height));
     this.vel=createVector(random(-5,5),random(-5,5));
     this.vel.mult(0.5);
+    this.mult1=Math.floor(random(1,20));
+    this.mult2=Math.floor(random(1,20));
+    this.mult=mult;
+    this.display=this.mult1*this.mult2;
     if (this.x) {
       this.pos.x=this.x;
     }
     if (this.y) {
       this.pos.y=this.y;
+    }
+    if (this.mult) {
+      this.display=this.mult;
     }
   }   //Roid
   update(){
@@ -23,8 +31,13 @@ class Roid {
     translate(this.pos.x,this.pos.y);
     fill('white');
     ellipse(0,0,this.radius*2);
-    imageMode(CENTER);
-    image(image1,0,0,this.radius*2+5,this.radius*2+5);
+     imageMode(CENTER);
+     image(image1,0,0,this.radius*2+5,this.radius*2+5);
+     fill(255);
+     stroke(27);
+     textSize(40);
+     
+  //   text(this.display,-20,15);
     pop();
   }   //Roid
   edges() {
