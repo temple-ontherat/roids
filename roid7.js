d8bab5e3406abce5a94151f63e6cee731be77f4a
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2023-03-04 20:48:31 -0500

    Update roid7.js
    Code for smaller asteroids.

--- roid7.js
+++ roid7.js
@@ -1,30 +1,23 @@
 class Roid {
-  constructor(rad,x,y,mult){
+  constructor(rad,x,y){
     this.radius=rad;
     this.x=x;
     this.y=y;
-    // this.pos=createVector(random(0,width/4),random(0,height/4));
     this.pos=createVector(random(0,width),random(0,height));
     this.vel=createVector(random(-5,5),random(-5,5));
     this.vel.mult(0.5);
-    this.mult1=Math.floor(random(1,20));
-    this.mult2=Math.floor(random(1,20));
-    this.mult=mult;
-    this.display=this.mult1*this.mult2;
     if (this.x) {
       this.pos.x=this.x;
     }
     if (this.y) {
       this.pos.y=this.y;
     }
-    if (this.mult) {
-      this.display=this.mult;
+    if (this.radius==10) {
+      this.vel.mult(1.5);
     }
   }   //Roid
   update(){
     this.pos.add(this.vel);
-  //  this.vel.mult(0.95);
-
   }
   show() {
     push();
@@ -36,8 +29,6 @@ class Roid {
      fill(255);
      stroke(27);
      textSize(40);
-     
-  //   text(this.display,-20,15);
     pop();
   }   //Roid
   edges() {
